85f36fe38c08717d4c625a680f04818a3cdcef77
LambdaXymox <lambda.xymox@gmail.com>
2024-07-27 21:47:38 -0500

    Minor refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -2379,7 +2379,7 @@ class App {
                 .minLod = 0.0f,
                 .maxLod = static_cast<float>(m_mipLevels),
                 .mipLodBias = 0.0f,
-                // Use there parameters to disable anisotropic filtering.
+                // // Use these parameters to disable anisotropic filtering.
                 // .anisotropyEnable = VK_FALSE,
                 // .maxAnisotropy = 1.0f,
             };
